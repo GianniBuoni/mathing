f265f67654f8b85abdcb60ef3cd40a98fb54eaa3
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-16 15:27:23 -0400

    Added Counter for totals
    - Removed mathStore since it was not used!

---
+++ src/components/react/Counter.tsx
@@ -0,0 +1,25 @@
+import useFormStore from "@/stores/useFormStore";
+
+const Counter = () => {
+  const { filledForms } = useFormStore();
+  const jonOnly = filledForms.filter((item) => item.payee === "jon");
+  const paulOnly = filledForms.filter((item) => item.payee === "paul");
+  const halved = filledForms.filter((item) => item.payee === "half");
+  const allJon = [...jonOnly, ...halved].reduce(
+    (acc, item) => acc + item.payeePrice,
+    0,
+  );
+  const allPaul = [...paulOnly, ...halved].reduce(
+    (acc, item) => acc + item.payeePrice,
+    0,
+  );
+  return (
+    <div className="flex gap-5">
+      <p>Jon: ${allJon.toFixed(2)}</p>
+      <p>Paul: ${allPaul.toFixed(2)}</p>
+      <p>Full ${(allPaul + allJon).toFixed(2)}</p>
+    </div>
+  );
+};
+
+export default Counter;
