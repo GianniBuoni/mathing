30f34a4ca3b445719b4cf9164511f5557adbc87e
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-16 15:30:26 -0400

    Some styling done
    TO DO:
    - Edit Items form
    - Cache store in browser (or store it in DB)

--- src/components/react/FilledFormArray.tsx
+++ src/components/react/FilledFormArray.tsx
@@ -8,7 +8,7 @@ const FilledFormArray = () => {
     modFilledForms(newArray);
   };
   return filledForms.map((form, i) => (
-    <div className="rounded-box bg-neutral p-5 flex gap-10 my-2 w-fit items-center">
+    <div className="rounded-box bg-neutral px-5 py-2 flex gap-10 my-2 w-fit items-center">
       <p>
         {form.item.item} : ${form.item.price}
       </p>
