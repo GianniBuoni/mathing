4e42ae90c6b872f1b3da2057fa41614c29c92877
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-17 22:32:36 -0400

    🎥 Removed AnimatePresence from form
    The cancel button was acting like a submit button under animate presence

--- src/components/react/FormCard.tsx
+++ src/components/react/FormCard.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import type { Variants } from "framer-motion";
 
 import useFormStore, { PayObjectSchema } from "@/stores/useFormStore";
@@ -32,53 +32,50 @@ const FormCard = () => {
   };
 
   return (
-    <AnimatePresence mode="popLayout">
-      {isFormActive && (
-        <motion.form
-          className="rounded-box bg-base-300 p-5 flex gap-10 w-fit items-center"
-          onSubmit={handleSubmit(onSubmit)}
-          key="formCard"
-          variants={variants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
+    isFormActive && (
+      <motion.form
+        className="rounded-box bg-base-300 p-5 flex gap-10 w-fit items-center"
+        onSubmit={handleSubmit(onSubmit)}
+        key="formCard"
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      >
+        <p>
+          {activeFormItem.item}: ${activeFormItem.price.toFixed(2)}
+        </p>
+        <label className="flex gap-3 items-center">
+          Quantity:
+          <input
+            type="number"
+            size={1}
+            defaultValue={1}
+            className="rounded p-1"
+            {...register("quantity")}
+          />
+        </label>
+        <select
+          id="payee"
+          defaultValue="choose"
+          className="rounded p-2"
+          {...register("payee")}
         >
-          <p>
-            {activeFormItem.item}: ${activeFormItem.price.toFixed(2)}
-          </p>
-          <label className="flex gap-3 items-center">
-            Quantity:
-            <input
-              type="number"
-              size={1}
-              defaultValue={1}
-              className="rounded p-1"
-              {...register("quantity")}
-            />
-          </label>
-          <select
-            id="payee"
-            defaultValue="choose"
-            className="rounded p-2"
-            {...register("payee")}
-          >
-            <option value="choose" disabled>
-              Who Pays?
-            </option>
-            <option value="jon">Jon</option>
-            <option value="paul">Paul</option>
-            <option value="half">Halvsies</option>
-          </select>
-          <SubmitCancelButtons />
-        </motion.form>
-      )}
-    </AnimatePresence>
+          <option value="choose" disabled>
+            Who Pays?
+          </option>
+          <option value="jon">Jon</option>
+          <option value="paul">Paul</option>
+          <option value="half">Halvsies</option>
+        </select>
+        <SubmitCancelButtons />
+      </motion.form>
+    )
   );
 };
 
 const variants: Variants = {
-  initial: { opacity: 0, y: -300 },
+  initial: { opacity: 0, y: -100 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -300 },
 };
 export default FormCard;
