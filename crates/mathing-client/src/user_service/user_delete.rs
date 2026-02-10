bfbf60b4456d7535e4213516d3edb599525120d5
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-10 12:28:17 -0500

    feat(client): client handles new proto types

--- crates/mathing-client/src/user_service/user_delete.rs
+++ crates/mathing-client/src/user_service/user_delete.rs
@@ -1,11 +1,13 @@
-use crate::prelude::mathing_proto::UserDeleteRequest;
+use crate::prelude::mathing_proto::{UserDeleteRequest, user_delete_request::OneOfId};
 
 use super::*;
 
 impl UserService {
-    pub(super) async fn handle_delete(&self, _args: UserArgs) -> anyhow::Result<()> {
+    pub(super) async fn handle_delete(&self, args: UserArgs) -> anyhow::Result<()> {
         // match name to uuid or return server error
-        let req = UserDeleteRequest { uuid: 1 };
+        let req = UserDeleteRequest {
+            one_of_id: Some(OneOfId::Name(args.name.to_string())),
+        };
         let res = self.connect().await?.user_delete(req).await?.into_inner();
         println!("RESPONSE: {:?}", res);
 
