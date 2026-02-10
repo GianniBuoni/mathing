bbecaa14e18217646828cf87dd00f176ab6bb021
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-10 16:23:38 -0500

    reafator(client): user new protobuf types

--- crates/mathing-client/src/user_service/user_delete.rs
+++ crates/mathing-client/src/user_service/user_delete.rs
@@ -1,4 +1,4 @@
-use crate::prelude::mathing_proto::{UserDeleteRequest, user_delete_request::OneOfId};
+use crate::prelude::mathing_proto::{OneOfId, UserDeleteRequest, one_of_id};
 
 use super::*;
 
@@ -6,7 +6,9 @@ impl UserService {
     pub(super) async fn handle_delete(&self, args: UserArgs) -> anyhow::Result<()> {
         // match name to uuid or return server error
         let req = UserDeleteRequest {
-            one_of_id: Some(OneOfId::Name(args.name.to_string())),
+            one_of_id: Some(OneOfId {
+                one_of_id: Some(one_of_id::OneOfId::Name(args.name.to_string())),
+            }),
         };
         let res = self.connect().await?.user_delete(req).await?.into_inner();
         println!("RESPONSE: {:?}", res);
