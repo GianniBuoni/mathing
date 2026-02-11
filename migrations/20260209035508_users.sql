3abac3297f29c0d656980f5167d548e8bf853de3
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-10 21:53:27 -0500

    refactor(db): add unique contraint to user names

--- migrations/20260209035508_users.sql
+++ migrations/20260209035508_users.sql
@@ -3,5 +3,5 @@ CREATE TABLE users (
   uuid UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   created_at TIMESTAMPTZ NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL,
-  name TEXT NOT NULL
+  name TEXT NOT NULL UNIQUE
 );
