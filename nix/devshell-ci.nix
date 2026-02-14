fea37ab7b7bde3f3cd66ce4c0d865523cf638734
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-14 12:56:02 -0500

    chore(ci): add shell test to lint task

--- nix/devshell-ci.nix
+++ nix/devshell-ci.nix
@@ -1,9 +1,19 @@
-{
-  flake.aspects.devshells.ci = {
-    devshell.startup.enterTest.text = ''
-      cargo -V;
-      cargo clippy -V;
-      just -V
-    '';
-  };
+{moduleWithSystem, ...}: {
+  flake.aspects.devshells.ci = moduleWithSystem ({pkgs, ...}: {
+    packages = with pkgs; [
+      just
+    ];
+
+    commands = [
+      {
+        name = "enterTest";
+        help = "Test CI lint enviornment";
+        command = ''
+          cargo -V;
+          cargo clippy -V;
+          just -V
+        '';
+      }
+    ];
+  });
 }
