de28e6d8936b60d3d1eed7824ed86cac1e720a34
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-17 13:41:19 -0500

    breaking(client): add -n flag to UserArgs

--- crates/mathing-client/src/cli.rs
+++ crates/mathing-client/src/cli.rs
@@ -17,21 +17,27 @@ pub enum Services {
 #[derive(Args)]
 pub struct UserArgs {
     pub action: CrudAction,
-    /// Required for all actions except 'list'
+    /// Target database entry to perform action on.
+    pub target: Option<Arc<str>>,
+    #[arg(long, short)]
+    /// Name value to pass to the 'create' and 'update' action
     pub name: Option<Arc<str>>,
 }
 
-#[derive(ValueEnum, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(ValueEnum, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub enum CrudAction {
     /// Creates a database entry for this entity; alias: 'add'
     #[value(alias = "add")]
     Create,
-    /// Deletes a database entry for this entity; alias: 'rm'
-    #[value(alias = "rm")]
-    Delete,
     /// Retrieve a single entry from the database
     Get,
     /// Retrieve all entries from the database; alias: 'ls'
     #[value(alias = "ls")]
     List,
+    #[value(alias = "edit")]
+    /// Update a database entry for this entity; alias: 'edit'
+    Update,
+    /// Deletes a database entry for this entity; alias: 'rm'
+    #[value(alias = "rm")]
+    Delete,
 }
