72a1130fb1d9ba126bdd84f6e7d4e558b5edd17d
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-17 11:22:34 -0500

    🔨 fix(client): update user_delete function to reflect protopuf changes

--- crates/mathing-client/src/user_service/user_delete.rs
+++ crates/mathing-client/src/user_service/user_delete.rs
@@ -1,20 +1,14 @@
-use crate::{
-    errors,
-    prelude::mathing_proto::{OneOfId, UserDeleteRequest, one_of_id},
-};
+use crate::{errors, prelude::mathing_proto::UserDeleteRequest};
 
 use super::*;
 
 impl UserService {
     pub(super) async fn handle_delete(&self, args: UserArgs) -> anyhow::Result<()> {
         let req = UserDeleteRequest {
-            one_of_id: Some(OneOfId {
-                one_of_id: Some(one_of_id::OneOfId::Name(
-                    args.name
-                        .ok_or_else(|| errors::clap_missing_arg("name"))?
-                        .to_string(),
-                )),
-            }),
+            target: args
+                .name
+                .ok_or_else(|| errors::clap_missing_arg("target"))?
+                .to_string(),
         };
         let rows = Into::<tabled::Table>::into(
             self.connect()
