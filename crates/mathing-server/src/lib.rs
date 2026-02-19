544261e801289f6a72deb27c82f00343b383add8
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-19 13:01:26 -0500

    feat(server): add bind limit constant for Db statements

--- crates/mathing-server/src/lib.rs
+++ crates/mathing-server/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod prelude {
+    /// Postgres bind limit for bulk staements
+    pub const BIND_LIMIT: usize = 65535;
     pub use std::sync::Arc;
     // Tonic wrapper types all messages should use.
     pub use tonic::{Request, Response, Status};
-    // Generated module that provides all message and service types
+    /// Generated module that provides all message and service types
     pub mod mathing_proto {
         tonic::include_proto!("mathing");
     }
